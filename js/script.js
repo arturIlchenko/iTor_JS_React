7a2d13079dbd22921a4c30168e2e973c08b3c52c
ArturIlchenko <artur.v.ilchenko@yandex.ru>
2022-08-18 22:34:17 +0500

    complete second homework

--- js/script.js
+++ js/script.js
@@ -2,28 +2,77 @@
 
 const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?'); // 1 задание
 
+
 const personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
 	actors: {},
 	genres: [],
 	privat: false
-}; // 2 задание
+};
 
-// let oneOfLastFilm = prompt('Один из последних просмотренных фильмов?');
-// let ratingLastFilm = prompt('На сколько оцените его?');
+if (personalMovieDB.count < 10) {
+	alert('Просмотрено довольно мало фильмов')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	alert('Вы - классический зритель');
+}  else if (personalMovieDB.count >= 30) {
+	alert('Вы-киноман')
+} else {
+	alert('Произошла ошибка')
+}
 
-// personalMovieDB['movies'][oneOfLastFilm] = ratingLastFilm;
+// 1 цикл
 
-// oneOfLastFilm = prompt('Один из последних просмотренных фильмов?');
-// ratingLastFilm = prompt('На сколько оцените его?');
+// for (let i = 0; i < 2; i++) {
+// 	const a = prompt('Один из последних просмотренных фильмов?');
+// 	if (a === '' || a === null || a.length > 50) {
+// 		alert('Вы не ввели название фильма! Попробуйте снова')
+// 		i--; 
+// 		continue};
+// 	const b = +prompt('На сколько оцените его?');
+// 	personalMovieDB['movies'][a] = b;
+// }
 
-const a = prompt('Один из последних просмотренных фильмов?'),
-		b = prompt('На сколько оцените его?'),
-		c = prompt('Один из последних просмотренных фильмов?'),
-		d = prompt('На сколько оцените его?');
+// 2 цикл
 
-personalMovieDB['movies'][a] = b;
-personalMovieDB['movies'][c] = d; // 3 задание
+// let i = 0;
+// while (i < 2) {
+// 	i++;
+// 	const a = prompt('Один из последних просмотренных фильмов?');
+// 	if (a === '' || a === null || a.length > 50) {
+// 		alert('Вы не ввели название фильма! Попробуйте снова')
+// 		i--; 
+// 		continue};
+// 	const b = +prompt('На сколько оцените его?');
+// 	personalMovieDB['movies'][a] = b;
+// }
+
+// 3 цикл
+
+let i = 0;
+do {
+	i++;
+	const a = prompt('Один из последних просмотренных фильмов?');
+	if (a === '' || a === null || a.length > 50) {
+		alert('Вы не ввели название фильма! Попробуйте снова')
+		i--; 
+		continue};
+	const b = +prompt('На сколько оцените его?');
+	personalMovieDB['movies'][a] = b;
+} while (i < 2);
 
 console.log(personalMovieDB);
+
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
